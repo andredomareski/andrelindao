9f5dfd10cb18d14f40a6720a9e85484e42062397
andre <108833211+andredomareski@users.noreply.github.com>
2024-04-02 11:14:38 -0300

    0204

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll("aba-conteudo");
-const contadores = document.querySelector(".contador")
-for (let i=0; i< botoes.length; i++){
+const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+
+for (let i=0; i < botoes.length; i++){
     botoes[i].onclick = function() {
         for (let j=0; j<botoes.length; j++){
             botoes[j].classList.remove ("ativo");
